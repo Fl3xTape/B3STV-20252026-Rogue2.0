0a2d27e9cc64ec4abb07275cad58edde341911bf
Fl3xTape <77446594+Fl3xTape@users.noreply.github.com>
2025-05-12 20:15:11 +0200

    Completed 7.1
    Please review the NUnit Test code

---
+++ STVrogue/NUnitTests/Test_Creature.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using STVrogue.GameLogic;
+using STVrogue.Utils;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class Test_Creature
+    {
+        [TestCase(Direction.NORTH)]
+        [TestCase(Direction.EAST)]
+        [TestCase(Direction.SOUTH)]
+        [TestCase(Direction.WEST)]
+        public void TestPlayerMove(Direction dir)
+        {
+            Room start = new Room("start", RoomType.ORDINARYroom, 5);
+            Room neighbor = new Room(dir.ToString().ToLower(), RoomType.ORDINARYroom, 5);
+            start.Connect(neighbor, dir);
+            
+            Player p = new Player("P0", "Player0");
+            p.Location = start;
+            p.Move(neighbor);
+
+            Assert.IsTrue(p.Location == neighbor);
+        }
+        
+        [TestCase(Direction.NORTH)]
+        [TestCase(Direction.EAST)]
+        [TestCase(Direction.SOUTH)]
+        [TestCase(Direction.WEST)]
+        public void TestMonterMove(Direction dir)
+        {
+            Room start = new Room("start", RoomType.ORDINARYroom, 5);
+            Room neighbor = new Room(dir.ToString().ToLower(), RoomType.ORDINARYroom, 5);
+            start.Connect(neighbor, dir);
+            
+            Monster m = new Monster("M0", "Monster0");
+            m.Location = start;
+            m.Move(neighbor);
+
+            Assert.IsTrue(m.Location == neighbor);
+        }
+        
+        [Test]
+        public void TestPlayerAttack()
+        {
+            Room room = new Room("room", RoomType.ORDINARYroom, 5);
+            Player p = new Player("P0", "Player0");
+            Monster m = new Monster("M0", "Monster0");
+            
+            p.Attack(m);
+            
+            Assert.IsTrue(m.Hp == 2);
+        }
+        
+        [Test]
+        public void TestMonsterAttack()
+        {
+            Room room = new Room("room", RoomType.ORDINARYroom, 5);
+            Player p = new Player("P0", "Player0");
+            Monster m = new Monster("M0", "Monster0");
+            
+            m.Attack(p);
+            
+            Assert.IsTrue(p.Hp == 19);
+        }
+    }
+}
