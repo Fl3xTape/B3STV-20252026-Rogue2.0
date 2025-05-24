685f2bc7ef191feb7f76213bd1b235074a98b41e
Stin Thielen <stin@hotmail.nl>
2025-05-24 15:59:12 +0200

    made randomAgent a seperate class

---
+++ STVrogue/STVrogue/RandomTestAgent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using STVrogue.GameLogic;
+
+namespace STVrogue;
+
+public class RandomTestAgent : TestAgent
+{
+    public RandomTestAgent() {
+
+    }
+    
+    public override char NextAction(List<string> consoleOutput, Game gameState) {
+        // This is the completely random text agent. only excluding the quit command.
+        int rand = new Random().Next(0, 6);
+        
+        switch (rand)
+        {
+            case 0:
+                return ' ';
+            case 1:
+                return 'm';
+            case 2:
+                return 'p';
+            case 3:
+                return 'a';
+            case 4:
+                return 'f';
+            case 5:
+                return 'u';
+            //case 6: lets not make the ai randomly quit
+            //return 'q';
+        }
+        
+        throw new NotImplementedException("Not supposed to reach here");
+    }
+}
